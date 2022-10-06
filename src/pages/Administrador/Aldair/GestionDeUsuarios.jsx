103ae448abc18d844aafa91a30ac6d6c4cc24b2d
Jherson Zuñiga <jherson.zuniga@pucp.edu.pe>
2022-10-05 21:52:52 -0500

    add

--- src/pages/Administrador/Aldair/GestionDeUsuarios.jsx
+++ src/pages/Administrador/Aldair/GestionDeUsuarios.jsx
@@ -1,23 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
 
 import { Select, Option } from "@material-tailwind/react";
 import { Input } from "@material-tailwind/react";
 import { Button } from "@material-tailwind/react";
 
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import DeleteIcon from '@mui/icons-material/Delete';
-import IconButton from '@mui/material/IconButton';
-import EditIcon from '@mui/icons-material/Edit';
-import Stack from '@mui/material/Stack';
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
+import Stack from "@mui/material/Stack";
 
-import {listarUsuarios} from '../../../services/UsuarioServices';
+import { listarUsuarios } from "../../../services/UsuarioServices";
 import DataTable from "react-data-table-component";
 
-const GestionDeUsuarios= () => {
-
+const GestionDeUsuarios = () => {
   const [search, setSearch] =
     useState(""); /*Default: muestra todo copia de users */
   const [filtered, setFiltered] = useState([]);
@@ -64,15 +63,11 @@ const GestionDeUsuarios= () => {
     },
   ];
 
-
   return (
-    <div
-      name="gestiondeusuarios"
-      className="h-screen w-full bg-white"
-    >
+    <div name="gestiondeusuarios" className="h-screen w-full bg-white">
       <div className="flex w-full h-20"></div>
       <div className="max-w-screen-lg p-8 mx-auto flex flex-col justify-start w-full h-full">
-        <div className="pb-10 mb-4 grid grid-cols-1">          
+        <div className="pb-10 mb-4 grid grid-cols-1">
           <p className="text-3xl font-semibold inline border-b-4 text-blue-pucp flex-auto border-blue-pucp">
             Gestión de Usuarios
           </p>
@@ -80,10 +75,10 @@ const GestionDeUsuarios= () => {
 
         <div className="pb-8 flex flex-row">
           <div className="w-72">
-            <Input 
-            label="Buscar" 
-            value={search}
-            onChange={(e) => setSearch(e.target.value)}
+            <Input
+              label="Buscar"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
             />
           </div>
           <div className="w-72  mx-12">
@@ -110,16 +105,14 @@ const GestionDeUsuarios= () => {
         </div>
 
         <div className="pb-6" style={{ height: 350, width: "100%" }}>
-          
-            <DataTable
-                columns={columnas}
-                data={filtered}
-                pagination
-                selectableRows
-                selectableRowsHighlight
-                highlightOnHover
-            ></DataTable>
-    
+          <DataTable
+            columns={columnas}
+            data={filtered}
+            pagination
+            selectableRows
+            selectableRowsHighlight
+            highlightOnHover
+          ></DataTable>
         </div>
       </div>
     </div>
